35a11c5f8e9806d0129e3c81391dbf2ca59accc2
Rohit grover <groverrohit54@gmail.com>
2024-05-29 12:06:55 +0530

    MorePages

--- blogs-context/src/context/AppContext.js
+++ blogs-context/src/context/AppContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { baseUrl } from "../baseUrl";
 
 export const AppContext = createContext();
@@ -8,11 +9,18 @@ export default function AppContextProvider({ children }) {
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(null);
+  const navigate = useNavigate();
 
   // Fetch Blog Data
-  const fetchBlogPosts = async (page = 1) => {
+  const fetchBlogPosts = async (page = 1, tag=null, category) => {
     setLoading(true);
     let url = `${baseUrl}?page=${page}`;
+    if(tag) {
+      url += `&tag=${tag}`;
+    }
+    if(category) {
+      url += `&category=${category}`;
+    }
     try {
       const res = await fetch(url);
       const data = await res.json();
@@ -33,9 +41,8 @@ export default function AppContextProvider({ children }) {
 
   // Handle When Next and Previous button are clicked
   const handlePageChange = (page) => {
+    navigate( { search: `?page=${page}`});
     setPage(page);
-    console.log(page);
-    fetchBlogPosts(page);
   };
 
   const value = {
